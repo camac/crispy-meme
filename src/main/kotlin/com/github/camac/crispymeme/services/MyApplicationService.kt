715200a940c940f17ae1b74b6c2f27c6bc88534d
GitHub Action <action@github.com>
2021-07-10 05:42:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/camac/crispymeme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/camac/crispymeme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.camac.crispymeme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.camac.crispymeme.MyBundle
 
 class MyApplicationService {
 
